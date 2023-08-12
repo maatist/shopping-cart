4f029d195988f54e3f61f167582b144c24f4fc51
Matias Sepulveda <matias.seto@gmail.com>
2023-08-12 13:34:29 -0400

    cambio a Fetch desde api dummyjson

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { Products } from './components/Products'
-import { products as initialProducts } from './mocks/products.json'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import { IS_DEVELOPMENT } from './config.JS'
@@ -9,31 +8,40 @@ import { Cart } from './components/Cart'
 import { CartProvider } from './context/cart'
 
 function App () {
-  const [products] = useState(initialProducts)
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [productos, setProductos] = useState(null)
 
   const {filterProducts} = useFilters()
-  const filteredProducts = filterProducts(products)
-
-  /* const [isLoading, setIsLoading] = useState(true)
 
    useEffect(() => {
-    fetch('https://dummyjson.com/products?limit=10&skip=0')
+    fetch('https://dummyjson.com/products?limit=20&skip=0')
     .then(response => response.json())
     .then(products => {
-      const [productos] = useState(products)
+      setProductos(products.products)
       setIsLoading(false)
     })
-  }, []) */
+  }, [])
 
-  return (
-    <CartProvider>
-      <Header />
-      <Cart />
-      <Products products={filteredProducts} />
-      {IS_DEVELOPMENT && <Footer />}
-
-    </CartProvider>
-  )
+  if (isLoading) { // ⬅️ si está cargando, mostramos un texto que lo indique
+    return (
+      <div className="App">
+        <h1>Cargando...</h1>
+      </div>
+    );
+  }
+  else {
+    const filteredProducts = filterProducts(productos)   
+      return (
+        <CartProvider>
+          <Header />
+          <Cart />
+          <Products products={filteredProducts} />
+          {IS_DEVELOPMENT && <Footer />}
+    
+        </CartProvider>
+      )  
+  }
 }
 
 export default App
